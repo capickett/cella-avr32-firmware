9ac1c2455bfab2574af92e57e0ce77ef2fa7c4d3
Cameron Pickett <pickett.cj@gmail.com>
2013-04-13 13:43:42 -0700

    Setup Blank project
    Cella Firmware.cproj is openable by Atmel Studio 6.0

---
+++ src/main.c
@@ -0,0 +1,19 @@
+/**
+ * \file
+ *
+ * \brief Empty user application template
+ *
+ */
+
+/*
+ * Include header files for all drivers that have been imported from
+ * Atmel Software Framework (ASF).
+ */
+#include <asf.h>
+
+int main (void)
+{
+	board_init();
+
+	// Insert application code here, after the board has been initialized.
+}
