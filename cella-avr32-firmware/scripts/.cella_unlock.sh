db8d0ca9003d588ccc4e6973802c76b8e87c20e2
Patrick Larson <palarson@cs.washington.edu>
2013-05-31 08:16:06 -0700

    Polished script, added input padding, length checking, lockfile

--- cella-avr32-firmware/scripts/.cella_unlock.sh
+++ cella-avr32-firmware/scripts/.cella_unlock.sh
@@ -1,7 +1,18 @@
 #! /bin/bash
 
-RESFILE="./RES"
-WRITEFILE="./CMD"
+#------------------------------------------#
+# CLI for Cella clients running Linux/Unix #
+#------------------------------------------#
+
+### begin constants ###
+
+RESFILE="./__cellaRES"
+CMDFILE="./__cellaCMD"
+LOCKFILE="./__cellaLOCK"
+MAX_PASS_LENGTH=32
+MAX_FAILED_ATTEMPTS=3
+
+### end constants ###
 
 ### begin functions ###
 
@@ -23,6 +34,27 @@ readresponse() {
     eval $__responseresult="'$resp'"
 }
 
+#-------------------------------------------------#
+# Echo input to outstream with length padded to   #
+# at least length.  If the input length is        #
+# greater than length, this simply prints input   #
+#                                                 #
+# parameters:                                     #
+#   $1 - input:   unpadded input                  #
+#   $2 - length:  minimum length of padded input  #
+#   $3 - newline: if true, end input with newline #
+#-------------------------------------------------#
+padinput() {
+    local index
+    echo -n "$1"
+    for (( index=${#1} ; index <= $2 ; index++ )) ; do
+        echo -ne "\0"
+    done
+    if $3; then
+        echo
+    fi
+}
+
 #------------------------------------------------------------------------#
 # Compares two strings and returns whether or not they are equal,        #
 # printing optional messages in either case                              #
@@ -37,16 +69,13 @@ readresponse() {
 validate() {
     local __validateresult=$3
     local myresult=false
-    if [ "$1" == "$2" ]
-      then
-        if [ "$4" != '' ]
-          then
+    if [ "$1" == "$2" ]; then
+        if [ -n "$4" ]; then
             echo -e "$4\n"
         fi
         myresult=true
     else
-        if [ "$5" != '' ]
-          then
+        if [ -n "$5" ]; then
             echo -e "$5\n"
         fi
         myresult=false
@@ -60,10 +89,7 @@ validate() {
 usage() {
     echo -e "c\tset configuration"
     echo -e "g\tget configuration"
-    echo -e "p\tunlock drive"
     echo -e "n\tchange password"
-    echo -e "?\tquery lock state"
-    echo -e "l\tlock drive"
 }
 
 #--------------------------------------------------------------------#
@@ -71,7 +97,9 @@ usage() {
 #--------------------------------------------------------------------#
 getconfig() {
     echo "Retrieving configuration..."
-    echo "c" > "$WRITEFILE"
+    touch "$LOCKFILE"
+    echo "c" > "$CMDFILE"
+    rm "$LOCKFILE"
     readresponse config
     local index; local option; local value
     for (( index=0; index<${#config}; index++ )); do
@@ -81,39 +109,64 @@ getconfig() {
              index=$(($index + 1))
              echo "Encryption level: $value"
             ;;
-        # new configuration options to be added here.
           *) # pass bad characters...
             ;;
         esac
     done
+    echo
+}
+
+#--------------------------------------------------------#
+# Lock drive, cleanup temporary files, and exit properly #
+#--------------------------------------------------------#
+graceful_exit() {
+    rm -f "$RESFILE"
+
+    # if we trap a signal in silent mode, the terminal gets stuck
+    # in silent mode. This hack ensures that doesn't happen
+    echo "uglyhack" | read
+    exit 0
 }
 
 ### end functions ###
 
 ### begin script ###
 
-INPUT="Cellaaaaaa!!!"
+input="Cellaaaaaa!!!"
 
-while [ "$INPUT" != '' ]; do
+trap graceful_exit SIGINT SIGTERM SIGHUP SIGSTOP
+
+failed=0
+while [ -n "$input" ]; do
     echo -e "Welcome to Cella Secure! Please enter your password. (newline to quit):"
-    read -p "> " -s INPUT
-    if [ "$INPUT" != '' ]; then
-        echo "$INPUT" > "$WRITEFILE"
-        echo -e "\nValidating..."
-        readresponse resp
-        validate "$resp" "K" result "Success!" "Validation failed."
-        if $result ; then
-            break
+    read -p "> " -s input
+    if [ -n "$input" ]; then
+        if [ ${#input} -le $MAX_PASS_LENGTH ]; then
+            touch "$LOCKFILE"
+            padinput "p$input" $MAX_PASS_LENGTH+1 true > "$CMDFILE"
+            rm "$LOCKFILE"
+            echo -e "\nValidating..."
+            readresponse resp
+            validate "$resp" "K" result "Success!" "Validation failed."
+            if $result ; then
+                break
+            elif [ $failed -ge $MAX_FAILED_ATTEMPTS ]; then
+                echo "Received too many incorrect passwords! Goodbye!"
+                exit 0
+            else
+                failed=$(($failed + 1))
+            fi
         else
-            exit
+            echo "Validation failed, please try again."
+            failed=$(($failed + 1))
         fi
     fi
 done
 
-while [ "$INPUT" != '' ]; do
+while [ -n "$input" ]; do
     echo -e "Enter a command (h for help, newline to quit):"
-    read -p "> " INPUT
-    case "$INPUT" in
+    read -p "> " input
+    case "$input" in
         "h") usage
             ;;
         "c") getconfig
@@ -125,15 +178,17 @@ while [ "$INPUT" != '' ]; do
                      "1") echo "Enter the new encryption level."
                           read -p "> " value
                           if [ $value == "0" ] || [ $value == "1" ] || [ $value == "2" ]; then
-                              newconfig="{$newconfig}e$value"
-                              echo "Encryption level is now $value"
+                              newconfig="e$value"
+                              echo "Encryption level will be set to $value"
                           else
                               echo "Invalid encryption level (Must be 0, 1, or 2)"
                           fi
                          ;;
                      # new configuration options to be added here.
                      "2") echo "Updating configuration..."
-                          echo "$newconfig" > "$WRITEFILE"
+                          touch "$LOCKFILE"
+                          echo "$newconfig" > "$CMDFILE"
+                          rm "$LOCKFILE"
                           readresponse resp
                           validate "$resp" "K" result "Update complete." "Update failed."
                           break
@@ -144,31 +199,30 @@ while [ "$INPUT" != '' ]; do
             ;;
         "g") getconfig
             ;;
-        "p") echo -e "Attempting to unlock, please enter password:"
-             read -p "> " -s PASS
-             echo "p$PASS" > "$WRITEFILE"
-             echo -e "Validating..."
-             readresponse resp
-             validate "$resp" "K" result "Your drive is now unlocked!" "Validation failed."
-            ;;
         "n") echo "Attemping to change password, please enter your OLD password."
              read -p "> " -s OLDPASS; echo
              NEWPASS1="foo"
              NEWPASS2="bar"
-             while [ "$NEWPASS" != "$OLDPASS" ] && [ "$OLDPASS" != '' ]; do
+             while [ "$NEWPASS1" != "$NEWPASS2" ] && [ -n "$OLDPASS" ]; do
                  echo "Please enter your NEW password (newline to abort)."
                  read -p "> " -s NEWPASS1; echo
-                 if [ "$NEWPASS1" != '' ]; then
+                 if [ -n "$NEWPASS1" ]; then
                      echo "Please enter your NEW password again."
                      read -p "> " -s NEWPASS2; echo
                  fi
-                 if [ "$NEWPASS1" == '' ] || [ "$NEWPASS2" == '' ]; then
+
+                 if [ -z "$NEWPASS1" ] || [ -z "$NEWPASS2" ]; then
                      echo -e "Aborting.\n"
                      break
+                 elif [ ${#NEWPASS1} -gt $MAX_PASS_LENGTH ] || [ ${#OLDPASS} -gt $MAX_PASSLENGTH ]; then
+                     echo -e "Your password is too long! Please keep it below 32 characters.\n"
                  elif [ "$NEWPASS1" != "$NEWPASS2" ]; then
                      echo -e "Your passwords did not match!\n"
                  else
-                     echo "n$OLDPASS$NEWPASS1" > "$WRITEFILE"
+                     touch "$LOCKFILE"
+                     padinput "n$OLDPASS" MAX_PASS_LENGTH+1 false > "$CMDFILE"
+                     padinput "$NEWPASS" MAX_PASS_LENGTH+1 true > "$CMDFILE"
+                     rm "$LOCKFILE"
                      echo "Validating..."
                      readresponse resp
                      validate "$resp" "K" result "Your password has been updated." "Validation failed!"
@@ -177,24 +231,9 @@ while [ "$INPUT" != '' ]; do
              done
              echo
             ;;
-        # hold up. This seems silly.
-        "?") echo "?" > "$WRITEFILE"
-             echo -e "Querying lock status..."
-             readresponse resp
-             validate "$resp" "?U" result "This drive is unlocked"
-             if [ ! $result ]; then
-                 validate "$resp" "?L" result "This drive is locked!" "Whoops, we got a bad response"
-             fi
-            ;;
-        "l") echo "l" > "$WRITEFILE"
-             echo -e "Locking drive..."
-             readresponse resp
-             validate "$resp" "K" result "Your drive is now locked." "Locking failed."
-            ;;
-         "") # echo "l" > "$WRITEFILE" # uncomment to lock drive on close
-             read -p "Are you sure you want to quit? (y/n) " conf
+         "") read -p "Are you sure you want to quit? (y/n) " conf
              if [ "$conf" == "y" ] || [ "$conf" == "yes" ]; then
-               echo "Goodbye!"
+               echo -e "Goodbye!\n"
              else
                 INPUT="cella!"
              fi
@@ -202,3 +241,4 @@ while [ "$INPUT" != '' ]; do
         *) echo "Unrecognized command: $c"
     esac
 done
+exit 0
