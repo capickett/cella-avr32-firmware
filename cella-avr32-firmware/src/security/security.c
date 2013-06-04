89c72ace398d01aeeedeae85dad07f436c0fa505
Matt Dorsett <madman2@cs.washington.edu>
2013-06-04 17:15:00 +0000

    Security enable

--- cella-avr32-firmware/src/security/security.c
+++ cella-avr32-firmware/src/security/security.c
@@ -62,8 +62,8 @@ void security_hash_pass_salt(uint8_t *password, uint8_t length, uint8_t *salt, u
 
 void security_flash_init()
 {
-	//if (!flashc_is_security_bit_active())
-		//flashc_activate_security_bit();
+	if (!flashc_is_security_bit_active())
+		flashc_activate_security_bit();
 }
 
 void security_factory_reset_init()
